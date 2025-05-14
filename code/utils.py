e52dc918558bb8eb498ddd1939a796daea6ec1d7
Firas Moosvi <firas@moosvi.com>
2025-05-14 09:23:07 -0700

    ready lecture2 demo

--- code/utils.py
+++ code/utils.py
@@ -43,7 +43,8 @@ def custom_plot_tree(tree_model, feature_names=None, class_names=None, **kwargs)
     
     # Customize the appearance of the text elements for each node
     for text in plt.gca().texts:
-        new_text = re.sub('samples = \d+\n', '', text.get_text()) # Hide samples
+        new_text = re.sub(r'samples = \d+\n', '', text.get_text())  # Hide samples
+
         text.set_text(new_text) 
     
     plt.show()
